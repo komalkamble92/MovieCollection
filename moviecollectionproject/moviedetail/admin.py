206c393890dbb183a5598ee588458dc172cc5107
komalkamble92 <komalkamble2592@gmail.com>
2024-10-08 12:13:16 +0530

    [add]

--- moviecollectionproject/moviedetail/admin.py
+++ moviecollectionproject/moviedetail/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+#from django.contrib import admin
+from .models import Movie, MovieCollection
+
+admin.site.register(Movie)
+admin.site.register(MovieCollection)
 
 # Register your models here.
