f32c99d8aaeee9e231f49f01b04e17c330040da8
komalkamble92 <komalkamble2592@gmail.com>
2024-10-08 15:43:55 +0530

    [add]

---
+++ collection/collectionofmovie/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CollectionofmovieConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'collectionofmovie'
--- moviecollectionproject/moviedetail/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-#from django.contrib import admin
-from .models import Movie, MovieCollection
-
-admin.site.register(Movie)
-admin.site.register(MovieCollection)
-
-# Register your models here.
